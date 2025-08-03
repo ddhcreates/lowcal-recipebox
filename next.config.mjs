bf36279629cf7aae729b919aa4b2432f8a486114
Dan Herbert <danherbert@hey.com>
2025-08-03 10:32:12 +0530

    next.config.mjs redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/ads.txt',
+        destination: 'https://srv.adstxtmanager.com/77226/lowcalrecipebox.com',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
