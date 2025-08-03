45f4a8cfc4076505f8c7736cd01ebdb6602c486e
Dan Herbert <danherbert@hey.com>
2025-08-03 23:35:46 +0530

    Vercel Analytics debig

--- app/layout.js
+++ app/layout.js
@@ -34,6 +34,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
